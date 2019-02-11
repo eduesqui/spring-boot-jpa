86db1bb594919656d3c966827148941e8920ae98
Eduardo <eduesqui87@gmail.com>
2019-02-10 20:39:10 -0600

    Added create user

--- spring-boot-jpa/src/main/java/com/eduesqui/springbootjpa/controllers/UserController.java
+++ spring-boot-jpa/src/main/java/com/eduesqui/springbootjpa/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.eduesqui.springbootjpa.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.eduesqui.springbootjpa.model.entitys.UserEntity;
 import com.eduesqui.springbootjpa.model.service.IUserBo;
 
 @Controller
@@ -19,6 +22,25 @@ public class UserController {
 		model.addAttribute("title", "Users");
 		model.addAttribute("users", userBo.findAll());
 		return "findUsers";
+		
+	}
+	
+	@RequestMapping(value="createUser", method=RequestMethod.GET)
+	public String createUser(Map<String, Object> model) {
+		model.put("title", "Create User");
+		UserEntity user = new UserEntity();
+		model.put("user", user);
+	
+		return "createUser";
+		
+	}
+	
+	
+	
+	@RequestMapping(value="saveUser", method=RequestMethod.POST)
+	public String createUser(UserEntity user) {
+		userBo.save(user);
+		return "redirect:findUsers";
 		
 	}
 
