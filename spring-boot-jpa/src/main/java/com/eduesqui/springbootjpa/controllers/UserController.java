bbb116f1a1082091c2a4b8cc4d204aa623a045e9
Eduardo <eduesqui87@gmail.com>
2019-02-18 21:46:37 -0600

    [ADD] Edit button

--- spring-boot-jpa/src/main/java/com/eduesqui/springbootjpa/controllers/UserController.java
+++ spring-boot-jpa/src/main/java/com/eduesqui/springbootjpa/controllers/UserController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,6 +19,7 @@ import com.eduesqui.springbootjpa.model.entitys.UserEntity;
 import com.eduesqui.springbootjpa.model.service.IUserBo;
 
 @Controller
+@SessionAttributes("userEntity")
 public class UserController {
 	
 	@Autowired
@@ -30,36 +33,11 @@ public class UserController {
 		
 	}
 	
-//	@RequestMapping(value="/createUser", method=RequestMethod.GET)
-//	public String createUser(Map<String, Object> model) {
-//		UserEntity user = new UserEntity();
-//		model.put("title", "Create User");
-//		model.put("user", user);
-//	
-//		return "createUser";
-//		
-//	}
-//	
-//	
-//	
-//	@RequestMapping(value="/createUser", method=RequestMethod.POST)
-//	public String saveUser(@Valid UserEntity user,  BindingResult result,Map<String, Object> model) {
-//		if (result.hasErrors()) {
-//			
-//			model.put("title", "Create User");
-//			model.put("user", user);
-//			
-//			
-//			return "createUser";
-//		}
-//		userBo.save(user);
-//		return "redirect:findUsers";
-//		
-//	}
+
 
 	
 	@RequestMapping(value = "/createUser")
-	public String crear(Map<String, Object> model) {
+	public String createUser(Map<String, Object> model) {
 
 		UserEntity userEntity = new UserEntity();
 		model.put("userEntity", userEntity);
@@ -68,15 +46,13 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
-	public String guardar(@Valid UserEntity userEntity, BindingResult result, Model model, SessionStatus status) {
+	public String createUser(@Valid UserEntity userEntity, BindingResult result, Model model, SessionStatus status) {
 		
 		if (result.hasErrors()) {
 			model.addAttribute("title", "Create user");
 			return "createUser";
 		}
 
-		
-		
 		if (userEntity.getName().equals("admin")) {
 			model.addAttribute("title", "Create user");
 			model.addAttribute("error", "No puede ser administrador");
@@ -84,8 +60,18 @@ public class UserController {
 		}
 		
 		userBo.save(userEntity);
-//		return "redirect:findUsers";  	
 		status.setComplete();
 		return "redirect:findUsers";
+	}
+	
+	@RequestMapping (value="/createUser/{id}" )
+	public String  editUser(@PathVariable (value="id")Long id,  Map<String, Object> model) {
+		if(id>0) {
+			model.put("userEntity", userBo.findOne(id));
+			model.put("title", "Create user");
+			return "createUser";
+		}else {
+			return "redirect:findUsers";
+		}
 	}
 }
--- spring-boot-jpa/src/main/java/com/eduesqui/springbootjpa/model/service/impl/UserBoImpl.java
+++ spring-boot-jpa/src/main/java/com/eduesqui/springbootjpa/model/service/impl/UserBoImpl.java
@@ -2,8 +2,11 @@ package com.eduesqui.springbootjpa.model.service.impl;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eduesqui.springbootjpa.model.dao.IUserDao;
 import com.eduesqui.springbootjpa.model.entitys.UserEntity;
@@ -17,25 +20,29 @@ public class UserBoImpl implements IUserBo{
 	private IUserDao userDao;
 	
 
+	@Transactional(readOnly = true)
 	@Override
 	public List<UserEntity> findAll() {
 		
 		return (List<UserEntity>) userDao.findAll();
 	}
 
+	@Transactional(readOnly = true)
 	@Override
 	public UserEntity findOne(Long id) {
 	
 		return userDao.findById(id).orElse(null);
 		//return userDao.findOne(id);
 	}
-
+	
+	@Transactional
 	@Override
 	public void save(UserEntity user) {
 		userDao.save(user);
 		
 	}
 
+	@Transactional
 	@Override
 	public void delete(Long id) {
 		userDao.deleteById(id);
